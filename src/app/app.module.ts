ffe2c5f51043c8cbd0660941223f59f5c6ffc2ca
Nathan Pickard <nathan.pickard87@gmail.com>
2019-09-05 12:59:26 -0700

    Header component, image assets, and album service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { LayoutModule } from '@progress/kendo-angular-layout';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
